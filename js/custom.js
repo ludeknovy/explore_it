f232bf014770cad6024434cf7d25bb4c57653839
Luděk Nový <novyludek@gmail.com>
2016-10-28 00:02:06 +0200

    stringear tab added

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,36 @@
 window.onload = function () {
-  new Clipboard('.copy');
+	new Clipboard('.copy');
 
-$(".copy").click(function() {
-  window.close();
+
+	$(".copy").click(function() {
+		window.close();
+	});
+
+	$("#textField").bind("change paste keyup", function() {
+		var x = $(this).val()
+		$("#size").html(x.length);
+
+
 });
+
+	$("#random-string").click(function() {
+		var length = document.getElementById("textLength").value;
+		if (length % 1 === 0) {
+			var random = "";
+			var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+			for( var i=0; i < length; i++ )
+				random += possible.charAt(Math.floor(Math.random() * possible.length));
+			$("#clipboard").css("visibility", "visible");
+			$("#resultGenerator").html(random) ;
+//			document.getElementById("resultGenerator").innerHTML = random;
+		} else {
+			$("#resultGenerator").html("invalid input");
+		}
+	})
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-  var c = new ProxyFormController( 'proxyForm' );
+	var c = new ProxyFormController( 'proxyForm' );
 });
+
+
