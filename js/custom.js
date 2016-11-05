24f2698853a40087e0ff89f1d88354f2c6d35ec6
Luděk Nový <novyludek@gmail.com>
2016-11-05 20:48:44 +0100

    data loading from son

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,9 @@
 window.onload = function () {
-	new Clipboard('.copy');
+
+	loadData();
 
 	$("#screen").click(function() {
-    chrome.extension.sendMessage({name: 'screenshot'})
-    });	
-
-	$(".copy").click(function() {
-		window.close();
+		chrome.extension.sendMessage({name: 'screenshot'})
 	});
 
 	$("#textField").bind("change paste keyup", function() {
@@ -34,7 +31,8 @@ window.onload = function () {
 		} else {
 			$("#string").val("invalid input");
 		}
-	});	
+	});
+
 };	
 
 
@@ -42,5 +40,30 @@ window.onload = function () {
 document.addEventListener('DOMContentLoaded', function () {
 	var c = new ProxyFormController( 'proxyForm' );
 });
+
+function loadData () {
+	chrome.runtime.sendMessage({name: 'json'}, function(response) {
+		var jsonData = JSON.parse(response.url);
+		$.each(jsonData, function(key, val) {
+			var img = val.icon;
+			var category = key;
+			var categoryID = category.replace(/ /g,'');
+			var categoryHtml = "<li id='" + categoryID +"' class='dropdown='><a href='#' class='dropdown-toggle lorem' data-toggle='dropdown'><img src='../../icons/" + img +"'>" + category + "<b class='caret'></b></a><ul class='dropdown-menu'>";
+			$(".mainPopup .nav.navbar-nav").append(categoryHtml);
+			$.each(val.items, function(itemKey, item) {
+				var tempItem = item;
+				var tempItemHtml = '<div class="col-lg-6"><div class="input-group"><button id="clipboard" class="btn btn-default copy clipboard-left" data-clipboard-target="#' + itemKey +'"><img src="../../icons/copy.svg"></button><span class="input-group-btn"><input id="' + itemKey +'" class="btn btn-default generate-input disabled input-value" value="' + item +'"></input></span></div></div>';
+				$('#' +categoryID + ' .dropdown-menu').append(tempItemHtml);
+			})
+		});
+
+		new Clipboard('.copy');
+
+		$(".copy").click(function() {
+			window.close();
+		});
+	});	
+};
+
 
 
